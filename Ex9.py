3968c9448289ec0b190a8e23c5f311b4df610d90
lhkhanh080720 <lehuukhanh872000@gmail.com>
2024-03-15 22:22:39 +0700

    final

--- Ex9.py
+++ Ex9.py
@@ -1,6 +1,6 @@
 a = int(input("Năm: "))
 
-if i % 400 == 0 or (i % 4 == 0 and i % 100 != 0):
+if a % 400 == 0 or (a % 4 == 0 and a % 100 != 0):
     print("Năm nhuận.")
 else: 
     print("Không phải là năm nhuận.")
